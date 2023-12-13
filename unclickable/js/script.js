eddc2fdb2093002a45c458fc112c84dcb6389859
RAJJIT <2.0lairaj@gmail.com>
2023-12-13 20:54:02 +0530

    fixes-02

--- unclickable/js/script.js
+++ unclickable/js/script.js
@@ -1,10 +1,25 @@
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+});
+
+
 const clickNo = document.getElementById('clickNo');
 clickNo.addEventListener('mouseover', moveHover)
 
-function moveHover(){
+function moveHover() {
     const first = Math.floor(Math.random() * 500) + 1;
     const second = Math.floor(Math.random() * 500) + 1;
 
     clickNo.style.left = first + "px";
     clickNo.style.top = second + "px";
+}
+
+popup = document.getElementById('popup');
+
+function openPopup() {
+    popup.classList.add('open-popup');
+}
+
+function closePopup() {
+    popup.classList.remove('open-popup');
 }
